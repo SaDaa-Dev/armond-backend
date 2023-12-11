b7a36135c26ba1e82775a733b07035b2a8d6ef0b
ChanBook <qudcks3000@gmail.com>
2023-12-11 23:44:43 +0900

    first Commit

---
+++ src/main/java/com/chandev/armond/ArmondApplication.java
@@ -0,0 +1,13 @@
+package com.chandev.armond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chandev/armond/ArmondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandev.armond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
