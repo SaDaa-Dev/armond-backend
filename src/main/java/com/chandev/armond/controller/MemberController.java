0e5019896a01d24f9289f9e7ff3d5ad88d19bd4e
ChanBook <qudcks3000@gmail.com>
2024-03-16 16:37:44 +0900

    컨트롤러 바로 Member 확장

--- src/main/java/com/chandev/armond/controller/MemberController.java
+++ src/main/java/com/chandev/armond/controller/MemberController.java
@@ -1,13 +1,29 @@
 package com.chandev.armond.controller;
 
+import com.chandev.armond.domain.Member;
+import com.chandev.armond.repository.MemberRepository;
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
     private final EntityManager em;
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/member/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
 
 }
