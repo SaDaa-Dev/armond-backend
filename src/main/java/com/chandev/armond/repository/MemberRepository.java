6d7106f4f268717698a5ee2a803ad6dfac49e984
ChanBook <qudcks3000@gmail.com>
2024-03-23 15:41:33 +0900

    jpa hint

--- src/main/java/com/chandev/armond/repository/MemberRepository.java
+++ src/main/java/com/chandev/armond/repository/MemberRepository.java
@@ -1,13 +1,11 @@
 package com.chandev.armond.repository;
 
 import com.chandev.armond.domain.Member;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -28,4 +26,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Override
     @EntityGraph(attributePaths = {"team"})
     List<Member> findAll();
+
+    // 100% 조회만 쓸 때 좀 더 최적화 할 수 있는 방법
+    @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
+    Member findReadOnlyByUsername(String username);
 }
