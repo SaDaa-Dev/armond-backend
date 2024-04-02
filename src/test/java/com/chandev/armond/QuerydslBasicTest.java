953dae793b4c2ca0267d82d51119bd9f83fb2c0a
ChanBook <qudcks3000@gmail.com>
2024-04-02 15:39:00 +0900

    querydsl fetchjoin

--- src/test/java/com/chandev/armond/QuerydslBasicTest.java
+++ src/test/java/com/chandev/armond/QuerydslBasicTest.java
@@ -2,9 +2,15 @@ package com.chandev.armond;
 
 import com.chandev.armond.entity.Member;
 import com.chandev.armond.entity.QMember;
+import com.chandev.armond.entity.QTeam;
 import com.chandev.armond.entity.Team;
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceUnit;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,7 +19,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static com.chandev.armond.entity.QMember.member;
+import static com.chandev.armond.entity.QTeam.team;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 @SpringBootTest
 @Transactional
@@ -51,14 +62,126 @@ public class QuerydslBasicTest {
 
     @Test
     void startQuerydsl() throws Exception {
-        QMember m = new QMember("m");
         Member findMember = queryFactory
-                .selectFrom(m)
-                .from(m)
-                .where(m.username.eq("member1"))
+                .selectFrom(member)
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
+
+    @Test
+    void search() throws Exception {
+        queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+    }
+
+    @Test
+    void resultFetchTest() throws Exception {
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        Member findFetch = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+
+        Member findFetch1 = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        long totalCnt = results.getTotal();
+        List<Member> fetchData = results.getResults();
+
+        long total = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+    }
+
+    @Test
+    void sort() throws Exception {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member .username.asc().nullsLast())
+                .fetch();
+    }
+
+    @Test
+    void aggregation() throws Exception {
+        queryFactory
+                .select(
+                        member.count(),
+                        member.age.sum()
+                )
+                .from(member)
+                .fetch();
+    }
+
+    @Test
+    void join() throws Exception {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+
+    }
+
+    @Test
+    void join_on_filtering() throws Exception {
+        JPAQuery<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team).on(team.name.eq("teamA"));
+    }
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+    @Test
+    void fetchJoinNo() throws Exception {
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("패치 조인 미적용").isFalse();
+
+
+    }
+
+    @Test
+    void fetchJoinUse() throws Exception {
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .join(member.team, team).fetchJoin()
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("패치 조인 적용").isTrue();
+
+
+    }
+
 
 }
