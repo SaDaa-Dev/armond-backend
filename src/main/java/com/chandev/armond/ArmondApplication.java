6dd1d2d997bbea1ad4212ae0dd0858a0104f084b
ChanBook <qudcks3000@gmail.com>
2024-04-04 10:21:18 +0900

    querydsl 조회 API Controller

--- src/main/java/com/chandev/armond/ArmondApplication.java
+++ src/main/java/com/chandev/armond/ArmondApplication.java
@@ -1,7 +1,10 @@
 package com.chandev.armond;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -10,6 +13,11 @@ public class ArmondApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArmondApplication.class, args);
+	}
+
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
 	}
 
 }
---
+++ src/main/java/com/chandev/armond/controller/MemberController.java
@@ -0,0 +1,20 @@
+package com.chandev.armond.controller;
+
+import com.chandev.armond.dto.MemberSearchCondition;
+import com.chandev.armond.dto.MemberTeamDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+}
